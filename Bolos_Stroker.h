81232b63bb3cc90c3d7f4cb1b11757e8ab6c9f76
LORENA PIN FRANCISCO <lorena.pin@alumnos.upm.es>
2026-05-02 18:31:32 +0200

    atributos personajes

--- Bolos_Stroker.h
+++ Bolos_Stroker.h
@@ -1,5 +1,12 @@
 #pragma once
 class Bolos_Stroker
 {
+private:
+    int vida;
+    int fuerza;
+    int defensa;
+    float velocidad;
+    float agilidad;
+    int estela;
 };
 
--- Boxeador_Normal.h
+++ Boxeador_Normal.h
@@ -1,5 +1,12 @@
 #pragma once
 class Boxeador_Normal
 {
+private:
+    int vida;
+    int fuerza;
+    int defensa;
+    float velocidad;
+    float agilidad;
+    int estela;
 };
 
